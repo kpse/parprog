84743946e0d08dbd3a6509be46d020ef09106242
Louis Suo <louis.suo@myob.com>
2017-01-18 12:20:55 +1100

    add vertical & horizontal blur

--- src/main/scala/scalashop/HorizontalBoxBlur.scala
+++ src/main/scala/scalashop/HorizontalBoxBlur.scala
@@ -3,6 +3,8 @@ package scalashop
 import org.scalameter._
 import common._
 
+import scala.collection.immutable.IndexedSeq
+
 object HorizontalBoxBlurRunner {
 
   val standardConfig = config(
@@ -10,7 +12,7 @@ object HorizontalBoxBlurRunner {
     Key.exec.maxWarmupRuns -> 10,
     Key.exec.benchRuns -> 10,
     Key.verbose -> true
-  ) withWarmer(new Warmer.Default)
+  ) withWarmer (new Warmer.Default)
 
   def main(args: Array[String]): Unit = {
     val radius = 3
@@ -37,26 +39,24 @@ object HorizontalBoxBlurRunner {
 object HorizontalBoxBlur {
 
   /** Blurs the rows of the source image `src` into the destination image `dst`,
-   *  starting with `from` and ending with `end` (non-inclusive).
-   *
-   *  Within each row, `blur` traverses the pixels by going from left to right.
-   */
+    * starting with `from` and ending with `end` (non-inclusive).
+    *
+    * Within each row, `blur` traverses the pixels by going from left to right.
+    */
   def blur(src: Img, dst: Img, from: Int, end: Int, radius: Int): Unit = {
-  // TODO implement this method using the `boxBlurKernel` method
-
-  ???
+    (from until end).foreach(row => (0 until src.width).foreach(col => dst.update(col, row, boxBlurKernel(src, col, row, radius))))
   }
 
   /** Blurs the rows of the source image in parallel using `numTasks` tasks.
-   *
-   *  Parallelization is done by stripping the source image `src` into
-   *  `numTasks` separate strips, where each strip is composed of some number of
-   *  rows.
-   */
+    *
+    * Parallelization is done by stripping the source image `src` into
+    * `numTasks` separate strips, where each strip is composed of some number of
+    * rows.
+    */
   def parBlur(src: Img, dst: Img, numTasks: Int, radius: Int): Unit = {
-  // TODO implement using the `task` construct and the `blur` method
+    // TODO implement using the `task` construct and the `blur` method
 
-  ???
+    //    blur(src, dst, 0, radius)
   }
 
 }
